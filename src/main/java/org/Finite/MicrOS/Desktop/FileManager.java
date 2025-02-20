d18f384fe320953aec3946adcaebc7974b32130b
charlie-san <charliesan@envs.net>
2025-02-20 19:33:06 +1100

    Add FileManager class for enhanced file management functionality

---
+++ src/main/java/org/Finite/MicrOS/Desktop/FileManager.java
@@ -0,0 +1,20 @@
+package org.Finite.MicrOS.Desktop;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import org.Finite.MicrOS.AsmRunner;
+import org.Finite.MicrOS.VirtualFileSystem;
+import org.Finite.MicrOS.WindowManager;
+import org.Finite.MicrOS.apps.ApplicationLauncher;
+
+public class FileManager {
+    
+}
